98b8fd395827ec922a99090cc2d6f33171704cff
Jack Harte <hartej2@cardiff.ac.uk>
2024-04-15 12:58:00 +0100

    Queue page auto update every 5 minutes.

--- src/pages/QueuePage.tsx
+++ src/pages/QueuePage.tsx
@@ -3,13 +3,15 @@ import {useGetAccessToken} from "../helpers/get_token.ts";
 import {useSelector} from "react-redux";
 import type {RootState} from "../redux/store.ts";
 import {selectHospital} from "../redux/hospitalSlice.ts";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import OuterBoxDiv from "../components/OuterBoxDiv.tsx";
 import LogoutButton from "../components/auth/LogoutButton.tsx";
 import {selectCheckedIn} from "../redux/checkedInSlice.ts";
 import {selectRole} from "../redux/roleSlice.ts";
 
 export const QueuePage = () => {
+    const [peopleInQueue, setPeopleInQueue] = useState(0);
+    const [initial, setInitial] = useState(true);
     const accessToken = useGetAccessToken("read:self");
     const hospital = useSelector((state: RootState) => selectHospital(state));
     const checkedIn = useSelector((state: RootState) => selectCheckedIn(state));
@@ -17,46 +19,57 @@ export const QueuePage = () => {
     console.log("Checked IN:" + checkedIn);
     console.log("Hospital:" + hospital)
     console.log(hospital)
-    console.log("Role:"  )
+    console.log("Role:")
     console.log(role)
 
     useEffect(() => {
-        if (!accessToken) {
-            return;
-        }
-
-        fetch('http://localhost:8080/waittime', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${accessToken}`
-            },
-            body: JSON.stringify({Hospital: hospital})
-        }).then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
+            if (!accessToken) {
+                return;
             }
-            return response.json();
+            const fetchData = () => {
+                fetch('http://localhost:8080/waittime', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${accessToken}`
+                    },
+                    body: JSON.stringify({Hospital: hospital})
+                }).then(response => {
+                    if (!response.ok) {
+                        throw new Error('Network response was not ok');
+                    }
+                    return response.json();
 
-        }).then(
-            data => {
-                console.log(data);
+                }).then(data => {
+                    setPeopleInQueue(data);
+                }).catch(error => {
+                    console.error('Error:', error);
+                });
+            };
+
+            if (initial) {
+                fetchData();
+                setInitial(false)
             }
-        ).catch(error => {
-            console.error('Error:', error);
-        });
-}, [accessToken, hospital]
-)
 
-return (
-    <div>
-    <LogoutButton/>
-    <OuterBoxDiv>
-        <CenteredBox>
-            <h1>Queue Page</h1>
-            <p>You are now waiting in the queue and saved in our system.</p>
-        </CenteredBox>
-    </OuterBoxDiv>
-    </div>
-);
+            const interval = setInterval(fetchData, 300000);
+
+            // Clear the interval when component unmounts
+
+            return () => clearInterval(interval);
+        }, [accessToken, hospital, initial]
+    )
+
+    return (
+        <div>
+            <LogoutButton/>
+            <OuterBoxDiv>
+                <CenteredBox>
+                    <h1>Queue Page</h1>
+                    <p>You are now waiting in the queue and saved in our system.</p>
+                    {peopleInQueue !== 0 && <p>There are {peopleInQueue} people in front of you.</p>}
+                </CenteredBox>
+            </OuterBoxDiv>
+        </div>
+    );
 }
